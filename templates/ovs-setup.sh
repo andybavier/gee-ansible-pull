93b9bd3c4a4304a710e1cc839312a8a62152ef70
Andy Bavier <acb@cs.princeton.edu>
2015-05-08 13:51:30 -0400

    Configure OVS

---
+++ templates/ovs-setup.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+MY_IP=$( hostname -i )
+
+create_tunnel {
+    HOST=$1
+    LABEL=$2
+
+    IFACE=gre$LABEL
+    IP=$( dig +short $HOST)
+    if ["$IP" = "$MY_IP"]; then return
+
+    ovs-vsctl add-port br-tun $IFACE
+    ovs-vsctl set interface $IFACE type=gre options:remote_ip=$IP
+    ovs-ofctl mod-port br-tun $IFACE noflood
+}
+
+no_arp_flood {
+    PORT=$( ovs-vsctl get interface docker0 ofport )
+    # These rules assume that all the GRE tunnel ports are in 'noflood' mode
+    ovs-ofctl add-flow br-tun in_port=$PORT,priority=20,dl_dst="ff:ff:ff:ff:ff:ff",action=all
+    ovs-ofctl add-flow br-tun priority=10,dl_dst="ff:ff:ff:ff:ff:ff",action=flood
+}
+
+{% for host in groups['nodes'] %}
+create_tunnel {{ hostvars[host]['inventory_hostname'] }} {{ hostvars[host]['label'] }}
+{% endfor %}
+
+no_arp_flood
+
