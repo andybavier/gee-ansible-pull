8ccf803077a0b5daef05b92cb2e205ce8352203d
Andy Bavier <acb@cs.princeton.edu>
2015-06-29 15:10:03 -0400

    Run script after Docker install

---
+++ scripts/post-install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# The Docker service starts automatically when installed.  This creates
+# docker0 bridge using the wrong IP address.  For some reason adding
+# /etc/default/docker before installing the Docker service doesn't seem
+# to work correctly.  So we'll fix the problem post-install.
+
+service docker stop
+
+ifconfig docker0 down || true
+brctl delbr docker0 || true
+
+service docker start
+/usr/local/sbin/ovs-setup.sh
